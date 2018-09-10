d8c4454ef228c63ed0028b1dab6b1f33fdb8318a
VadimRydenko <rudenkovvv@ukr.net>
2018-09-10 15:00:29 +0300

    Test commit

---
+++ src/lesson-10/lesson-10.js
@@ -0,0 +1,11 @@
+var userFirstName = " Vadim";
+var userSecondName = " Rudenko";
+var userAge = 25;
+
+var user = {
+    firstName: prompt('Enter you first name'),
+    secondName: prompt('Enter you second name'),
+    age: prompt('Enter you age'),
+}
+
+alert (user.firstName + ' ' +user.secondName + '' + user.age);
